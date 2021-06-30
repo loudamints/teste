952fccad88d71c00dbbee77c8992fb79d30a8b26
lidiaganhito <lidiaganhito@users.noreply.github.com>
2021-06-29 21:49:34 -0300

    third push

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import { useState } from 'react';
 function Home() {
 	return (
 		<div>
-			<h2>Eae, querida Lidiane? Agora tamo no stage.</h2>
+			<h2>Eae, querida Lidiane?</h2>
+			<h2>Beleuzis?</h2>
+			<h3>Testando as parada aqui</h3>
 		</div>
 	)
 }
